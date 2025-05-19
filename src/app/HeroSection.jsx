10ac7f541844bc38d09880a0dc256b49bca833e3
Mehboob Hassan <mhgajju@gmail.com>
2025-05-19 08:43:08 +0500

    First Commit

---
+++ src/app/HeroSection.jsx
@@ -0,0 +1,21 @@
+const HeroSection = () => {
+    return (
+        <div className="hero-section">
+            <div className="hero-section-content">
+                <h1>Create Stunning outdoor Gardens with Expert Care</h1>
+                <p>Discover the Art of Sustainable and Beautiful Landscapes</p>
+                <div className="hero-section-buttons">
+                    <button className="styled-button">Get a Free Quote</button>
+                    <button className="transparent-button">Call Us Now</button>
+                </div>
+            </div>
+            <div className="hero-section-image">
+                <img src="/images/furry-pet2.png" alt="Garden" />
+
+            </div>
+        </div>
+    );
+}
+
+
+export default HeroSection;
